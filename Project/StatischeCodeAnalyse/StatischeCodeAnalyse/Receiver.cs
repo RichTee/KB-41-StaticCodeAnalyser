f473ed0db1c67e8a2a8bacc51bcf482e44a709f0
Jelmer Groeneveld <jelmer@fluxility.com>
2017-09-20 12:43:17 +0200

    Write arguments in console

--- Project/StatischeCodeAnalyse/StatischeCodeAnalyse/Receiver.cs
+++ Project/StatischeCodeAnalyse/StatischeCodeAnalyse/Receiver.cs
@@ -4,15 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Mono.Options;
 
 
 namespace StatischeCodeAnalyse
 {
     class Receiver
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Debug.WriteLine("Hello Debug Output");
+            Console.WriteLine("Amount of arguments: " + args.Length);
+
+            foreach(string a in args) 
+            {
+                Console.WriteLine(a);
+            }
         }
     }
 }
