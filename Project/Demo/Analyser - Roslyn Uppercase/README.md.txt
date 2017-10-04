5fc9540c27543ae569dd77db0aaabcb298b23f79
RichTee <hans@rosterbuster.com>
2017-10-04 12:38:20 +0200

    Add demo Roslyn Analyser + Code FIx

---
+++ Project/Demo/Analyser - Roslyn Uppercase/README.md.txt
@@ -0,0 +1,5 @@
+How to run:
+1. Open the Analyzer Project in Visual Studio
+2. Debug -> Start without Debugging
+3. Open the Test Project in the new Visual Studio window
+4. Hover over the class1 name and see that it suggests a code fix to make it capital letters
---
+++ Project/Demo/Analyser - Roslyn Uppercase/Test/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalyserUppercaseTest
+{
+    public class Class1
+    {
+        // The class1 should show a fix with uppercase
+    }
+}
