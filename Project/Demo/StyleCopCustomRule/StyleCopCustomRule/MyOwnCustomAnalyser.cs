dd33554ca7c03c40dc6e32d6da0c87499f73568d
Jelmer Groeneveld <jelmer_groeneveld@live.nl>
2017-10-04 15:34:26 +0200

    Mono.Cecil and StyleCop trials

---
+++ Project/Demo/StyleCopCustomRule/StyleCopCustomRule/MyOwnCustomAnalyser.cs
@@ -0,0 +1,54 @@
+﻿namespace StyleCopCustomRule
+{
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    [SourceAnalyzer(typeof(CsParser))]
+    public class MyOwnCustomAnalyzer : SourceAnalyzer
+    {
+        public override void AnalyzeDocument(CodeDocument currentCodeDocument)
+        {
+            var codeDocument = (CsDocument)currentCodeDocument;
+            if (codeDocument.RootElement != null && !codeDocument.RootElement.Generated)
+            {
+                codeDocument.WalkDocument(new CodeWalkerElementVisitor<object>(this.InspectCurrentElement), null, null);
+            }
+        }
+
+        private bool InspectCurrentElement(CsElement element, CsElement parentElement, object context)
+        {
+            if (element.ElementType == ElementType.Method)
+            {
+                bool boolIsTryFound = false;
+                bool boolIsCatchFound = false;
+
+                var tempDocument = (CsDocument)element.Document;
+                var objReader = tempDocument.SourceCode.Read();
+
+                string strCode;
+                while ((strCode = objReader.ReadLine()) != null)
+                {
+                    if (strCode.Contains("try"))
+                    {
+                        boolIsTryFound = true;
+                    }
+
+                    if (boolIsTryFound)
+                    {
+                        if (strCode.Contains("catch"))
+                        {
+                            boolIsCatchFound = true;
+                        }
+                    }
+                }
+
+                if ((boolIsTryFound) && (boolIsCatchFound == false))
+                {
+                    this.AddViolation(parentElement, "MyOwnCustomRule", "CatchShouldBeImplemented");
+                }
+            }
+            return true;
+        }
+
+    }
+}
