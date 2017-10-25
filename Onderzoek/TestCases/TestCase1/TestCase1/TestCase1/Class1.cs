e53aaa12c4bc447d3f33461f1ab3a24a6067a82e
RichTee <hans@rosterbuster.com>
2017-10-25 12:26:05 +0200

    Add testcases

---
+++ Onderzoek/TestCases/TestCase1/TestCase1/TestCase1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCase1
+{
+    public class TestCase1
+    {
+        public TestCase1()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Onderzoek/TestCases/TestCase2/TestCase2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCase2
+{
+    public class Class1
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
